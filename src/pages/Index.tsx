dffa1b7de4fe3eb74af43a7af77db65384b17c05
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-05 20:12:49 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,63 +12,63 @@ const Index = () => {
     <>
       <Helmet>
         {/* Titel & Meta Description */}
-        <title>Barbershop Prenzlauer Berg | The Gentleman's Cut</title>
+        <title>Friseursalon Karizma | Ihr Friseur in Aachen</title>
         <meta
           name="description"
-          content="Besuche unseren Premium Barbershop The Gentleman's Cut in Berlin Prenzlauer Berg für erstklassige Haarschnitte, Bartpflege und individuelle Styles. Jetzt Termin buchen!"
+          content="Besuchen Sie den Friseursalon Karizma in Aachen für erstklassige Haarschnitte, Bartpflege und individuelle Styles. Erleben Sie professionelles Handwerk. Jetzt Termin buchen!"
         />
         <meta
           name="keywords"
-          content="Barbershop, Herrenfriseur, Haarschnitt, Bartpflege, Rasur, Berlin, Prenzlauer Berg"
+          content="Friseur, Barbershop, Herrenfriseur, Haarschnitt, Bartpflege, Rasur, Aachen"
         />
-        <link rel="canonical" href="https://gentlemanscut.de" />
+        <link rel="canonical" href="https://friseursalon-karizma.de" />
 
         {/* Open Graph */}
-        <meta property="og:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta property="og:title" content="Friseursalon Karizma | Premium Friseur & Barber Aachen" />
         <meta
           property="og:description"
-          content="Erstklassige Haarschnitte und Bartpflege für den modernen Gentleman in Berlin Prenzlauer Berg."
+          content="Erstklassige Haarschnitte und Bartpflege für den modernen Look im Friseursalon Karizma in Aachen."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://gentlemanscut.de" />
-        <meta property="og:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta property="og:url" content="https://friseursalon-karizma.de" />
+        <meta property="og:image" content="https://friseursalon-karizma.de/images/og-image.png" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta name="twitter:title" content="Friseursalon Karizma | Premium Friseur Aachen" />
         <meta
           name="twitter:description"
-          content="Erleben Sie erstklassige Haarschnitte und Bartpflege für den modernen Gentleman."
+          content="Erleben Sie erstklassige Haarschnitte und Bartpflege im Friseursalon Karizma."
         />
-        <meta name="twitter:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta name="twitter:image" content="https://friseursalon-karizma.de/images/og-image.png" />
 
         {/* Schema.org LocalBusiness */}
         <script type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "Barbershop",
-            "name": "The Gentleman's Cut",
-            "image": "https://gentlemanscut.de/images/logo.png",
+            "@type": "HairSalon",
+            "name": "Friseursalon Karizma",
+            "image": "https://friseursalon-karizma.de/images/logo.png",
             "address": {
               "@type": "PostalAddress",
-              "streetAddress": "Straße 12",
-              "addressLocality": "Berlin",
-              "postalCode": "10115",
+              "streetAddress": "Alexanderstraße 69",
+              "addressLocality": "Aachen",
+              "postalCode": "52062",
               "addressCountry": "DE"
             },
             "geo": {
               "@type": "GeoCoordinates",
-              "latitude": "52.5200",
-              "longitude": "13.4050"
+              "latitude": "50.7753",
+              "longitude": "6.0839"
             },
-            "url": "https://gentlemanscut.de",
-            "telephone": "+49 30 1234567",
-            "openingHours": "Mo-Fr 09:00-19:00, Sa 09:00-17:00",
+            "url": "https://friseursalon-karizma.de",
+            "telephone": "+49 176 29151040",
+            "openingHours": "Di-Fr 09:00-19:00, Sa 09:00-16:00",
             "priceRange": "€€",
-            "servesCuisine": "Barbershop Services",
+            "servesCuisine": "Friseur & Barber Services",
             "sameAs": [
-              "https://www.facebook.com/gentlemanscut",
-              "https://www.instagram.com/gentlemanscut"
+              "https://www.facebook.com/friseursalonkarizma",
+              "https://www.instagram.com/friseursalonkarizma"
             ]
           })}
         </script>
